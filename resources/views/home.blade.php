4aceee485eb69b14e95abfa8b4d388c2254d4070
nicola1397 <nicola1397@outlook.it>
2024-03-17 22:03:23 +0100

    feat: added base navbar items

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,16 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="ie=edge" http-equiv="X-UA-Compatible">
-    <title>Document</title>
+    <title>DC Comics</title>
+    <link rel="icon" type="image/x-icon" href="/images/favicon.ico">
 
     @vite('resources/js/app.js')
   </head>
 
   <body>
-    <h1>Laravel with Bootstrap + Vite</h1>
+@include("partials.header")
+
+      <h1>Funonza</h1>
   </body>
 
 </html>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,56 @@
+<header>
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+        <img src="../images/dc-logo.png" alt="Logo">
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+      <li class="nav-item">  
+      <a class="nav-link" aria-current="page" href="#">CHARACTER</a>
+        </li><li class="nav-item">
+        <a class="nav-link" href="#">COMICS</a>
+        </li><li class="nav-item">
+        <a class="nav-link" href="#">MOVIES</a>
+        </li><li class="nav-item">
+        <a class="nav-link" href="#">TV</a>
+        </li><li class="nav-item">
+        <a class="nav-link" href="#">GAMES</a>
+        </li><li class="nav-item">
+        <a class="nav-link" href="#">COLLECTIBLES</a> 
+        </li><li class="nav-item">
+        <a class="nav-link" href="#">VIDEOS</a>
+        </li><li class="nav-item">
+        <a class="nav-link" href="#">FANS</a>
+        </li><li class="nav-item">
+        <a class="nav-link" href="#">NEWS</a>
+        </li><li class="nav-item">
+        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+SHOP          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">1</a></li>
+            <li><a class="dropdown-item" href="#">2</a></li>
+            <li><a class="dropdown-item" href="#">3</a></li>
+          </ul></li>
+
+          <div class="searchBar">
+            <input
+              class="form-control border-0"
+              type="text"
+              placeholder="Search..."
+              id="search-input"
+            />
+            <span class="searchIcon">
+              <button class="btn ms-n3" type="button">
+                <i class="bi bi-search"></i>
+              </button>
+            </span>
+          </div>
+      </div>
+    </div>
+  </div>
+</nav>
+</header>
