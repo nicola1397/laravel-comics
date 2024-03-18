e7fa2ba8b4fb21f027a4657f66773e9ec54dd68c
nicola1397 <nicola1397@outlook.it>
2024-03-18 22:43:08 +0100

    feat: added utilities in comics page

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -33,7 +33,24 @@
             </div>
             
         </div>    
-        
+</div>
+
+<div id="utilities">
+  <div class="container-md">
+    <div class="row justify-content-between">
+      <div class="col-auto"><a href=""><img src="../images/buy-comics-digital-comics.png" alt="">
+    <span>DIGITAL COMICS</span></a></div>
+    <div class="col-auto"><a href=""><img src="../images/buy-comics-merchandise.png" alt="">
+    <span>DC MERCHANDISE</span></a></div>
+    <div class="col-auto"><a href=""><img src="../images/buy-comics-subscriptions.png" alt="">
+    <span>SUBSCRIPTIONS</span></a></div>
+    <div class="col-auto"><a href=""><img src="../images/buy-comics-shop-locator.png" alt="">
+    <span>COMIC SHOP LOCATOR</span></a></div>
+    <div class="col-auto"><a href=""><img src="../images/buy-dc-power-visa.svg" alt="">
+    <span>DC POWER VISA</span></a></div>
+    </div>
+  </div>
+
 </div>
 
 @endsection
